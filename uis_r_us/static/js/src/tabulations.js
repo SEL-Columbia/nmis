fbc7ed54571d0ce89850a78c4ba99b7ab0469e08
Alex Dorey <dorey415@gmail.com>
2011-09-09 11:04:12 -0400

    more testing of "tabulations" stuff.

--- uis_r_us/static/js/src/tabulations.js
+++ uis_r_us/static/js/src/tabulations.js
@@ -4,6 +4,11 @@ var Tabulation = (function(){
         data = _data;
         return true;
     }
+    function ensureData() {
+        if(data===undefined) {
+            warn("Data is undefined");
+        }
+    }
     function bySector (sector) {
         return _.map(data, function(d){
             return 3;
@@ -19,18 +24,35 @@ var Tabulation = (function(){
     }
     function sectorSlug (sector, slug, keys) {
         var occurrences = {};
-        var values = _.pluck(filterBySector(sector), slug);
-        if(keys===undefined) keys = _.uniq(values);
+        var values = _(filterBySector(sector)).chain()
+                        .pluck(slug)
+                        .map(function(v){
+                            return '' + v;
+                        })
+                        .value();
+        if(keys===undefined) keys = _.uniq(values).sort();
         _.each(keys, function(key) { occurrences[key] = 0; });
         _.each(values, function(d){
             if(occurrences[d] !== undefined)
                 occurrences[d]++;
         });
+        log(JSON.stringify(occurrences));
         return occurrences;
+    }
+    function sectorSlugAsArray (sector, slug, keys) {
+        var occurrences = sectorSlug.apply(this, arguments);
+        if(keys===undefined) { keys = _.keys(occurrences).sort(); }
+        return _(keys).map(function(key){
+            return {
+                occurrences: '' + key,
+                value: occurrences[key]
+            };
+        });
     }
     return {
         init: init,
         sectorSlug: sectorSlug,
+        sectorSlugAsArray: sectorSlugAsArray,
         bySector: bySector
     };
 })();
--- uis_r_us/static/js/src/tabulations_test.js
+++ uis_r_us/static/js/src/tabulations_test.js
@@ -10,9 +10,24 @@ _.times(40, function(i){
 test("Tabulations work with sample data", function(){
     ok(Tabulation.init(data), "Tabulation.init(data) returned true");
     equal(Tabulation.bySector().length, 40);
-    deepEqual(Tabulation.sectorSlug("education", "something"),
-            {
-                'true': 3,
-                'false': 7
-            });
+    deepEqual(Tabulation.sectorSlug("education", "something"), {
+        'false': 7,
+        'true': 3
+    }, "Tabulation.sectorSlug correctly calculates the tabulations.");
+    deepEqual(Tabulation.sectorSlugAsArray("education", "something"),
+            [
+                {
+                    occurrences: 'false',
+                    value: 7
+                },
+                {
+                    occurrences: 'true',
+                    value: 3
+                }
+            ], "Tabulation.sectorSlugAsArray (which is used once) maps the tabulations into an array.");
+    deepEqual(Tabulation.sectorSlug("education", "something", ["true", "false", "maybe"]), {
+        'true': 3,
+        'false': 7,
+        'maybe': 0
+    }, "Tabulation.sectorSlug can receive a list of keys and will include values as 0 if not found.")
 });
