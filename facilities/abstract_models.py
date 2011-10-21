64f822c0a9570b686efded522efbdc6beb448a85
Mark Johnston <mejymejy@gmail.com>
2011-10-21 16:18:18 -0400

    try to prevent key errors when calculating values from killing formulas

--- facilities/abstract_models.py
+++ facilities/abstract_models.py
@@ -166,6 +166,10 @@ class CalculatedVariable(Variable):
             d[self.slug] = value
 
     def calculate_value(self, d):
+        from collections import defaultdict
+        def _none():
+            return None
+        d = defaultdict(_none, d)
         try:
             return eval(self.formula)
         except:
