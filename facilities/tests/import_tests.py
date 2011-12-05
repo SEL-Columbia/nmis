2aaf9fc7b687339896772add74168d986abaa14e
Alex Dorey <dorey415@gmail.com>
2011-12-05 01:23:19 -0500

    removed test of "data_loader.create_facility_types()"

--- facilities/tests/import_tests.py
+++ facilities/tests/import_tests.py
@@ -19,24 +19,6 @@ class ImportDataTest(TestCase):
             }
         self.assertEquals(sectors, expected_dict)
 
-    def test_create_facility_types(self):
-        self.data_loader.create_facility_types()
-        self.assertEquals(FacilityType.objects.count(), 23)
-
-    def test_limited_import(self):
-        # this needs to be rewritten with small csvs
-
-        # data_loader.load()
-        # expected_info = {
-        #     "number of facilities": 41,
-        #     "unused variables": [],
-        #     "facilities without lgas": 0,
-        #     "number of lga records": 183,
-        #     "number of facility records": 4419
-        #     }
-        # self.assertEquals(data_loader.get_info(), expected_info)
-        pass
-
     def count_all_objects(self):
         result = {}
         for ct in ContentType.objects.all():
