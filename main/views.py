c5e4be593f13d83923a0585950eb4d948c6f6ff8
myf <worldwidespirit@gmail.com>
2013-09-30 21:06:38 -0400

    adding home, download and about pages, move css to static but separ

--- main/views.py
+++ main/views.py
@@ -5,9 +5,6 @@ from django.http import HttpResponse, HttpResponseBadRequest,\
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
-import json
-import os
-import re
 
 def homepage(request):
   return render_to_response('homepage.html',{
@@ -15,3 +12,13 @@ def homepage(request):
                             context_instance=RequestContext(request))
 
 
+def download(request):
+  return render_to_response('data_download.html',{
+                            },
+                            context_instance=RequestContext(request))
+
+
+def about(request):
+  return render_to_response('about2.html',{
+                            },
+                            context_instance=RequestContext(request))
