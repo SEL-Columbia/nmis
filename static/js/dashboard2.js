4ee4488373934ff5f5c28eb16ad4125e06f5ce66
Chris Tan <csytan@gmail.com>
2013-10-08 17:13:26 -0400

    Added initial backbone routes.

--- static/js/dashboard2.js
+++ static/js/dashboard2.js
@@ -1,9 +1,23 @@
 $(function(){
-	var template = _.template($('#country_view_tmpl').html());
-	var html = template({zones: ZONES});
-	$('#content').append(html);
+	new Backbone.Router({
+		routes: {
+			'': country_view,
+			//':unique_lga': lga_overview,
+			//':unique_lga/health': lga_health,
+			//':unique_lga/education': lga_education,
+			//':unique_lga/water': lga_water,
+			//':unique_lga/facility': facility_overview,
+			//':unique_lga/facility_health': facility_health,
+			//':unique_lga/facility_education': facility_education,	
+			//':unique_lga/facility_water': facility_water
+		}
+	});
+	Backbone.history.start();
 
-
-
+	function country_view(){
+		var template = _.template($('#country_view_template').html());
+		var html = template({zones: ZONES});
+		$('#content').append(html);
+	}
 });
 
