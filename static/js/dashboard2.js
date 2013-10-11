788b2f6b9f07e46aef19c63046dea12ec4d727f1
Myf Ma <myf@worldtalks.org>
2013-10-11 12:05:34 -0400

    syntax

--- static/js/dashboard2.js
+++ static/js/dashboard2.js
@@ -1,84 +1,84 @@
 (function(){
-	NMIS = {};
-	var template_cache = {};
-	var views = {};
+  NMIS = {};
+  var template_cache = {};
+  var views = {};
 
-	$(function(){
-		new Backbone.Router({
-			routes: {
-				'': index,
-				':unique_lga': route(lga_overview),
-				':unique_lga/health': route(lga_sector, 'health'),				
-				':unique_lga/education': route(lga_sector, 'education'),
-				':unique_lga/water': route(lga_sector, 'water'),
-				':unique_lga/facility': route(facility_overview),
-				//':unique_lga/facility_health': facility_health,
-				//':unique_lga/facility_education': facility_education,	
-				//':unique_lga/facility_water': facility_water
-			}
-		});
-		Backbone.history.start();
-	});
-	
+  $(function(){
+    new Backbone.Router({
+      routes: {
+        '': index,
+        ':unique_lga': route(lga_overview),
+        ':unique_lga/health': route(lga_sector, 'health'),        
+        ':unique_lga/education': route(lga_sector, 'education'),
+        ':unique_lga/water': route(lga_sector, 'water'),
+        ':unique_lga/facility': route(facility_overview),
+        //':unique_lga/facility_health': facility_health,
+        //':unique_lga/facility_education': facility_education,  
+        //':unique_lga/facility_water': facility_water
+      }
+    });
+    Backbone.history.start();
+  });
+  
 
-	// Page Views
-	function index(){
-		render('#index_template', {});
-	}
+  // Page Views
+  function index(){
+    render('#index_template', {});
+  }
 
-	function lga_overview(lga){
-		render('#lga_overview_template', {
-			lga: lga.lga_data
-		});
+  function lga_overview(lga){
+    render('#lga_overview_template', {
+      lga: lga.lga_data
+    });
 
-		leaflet_overview(lga);
-	}
+    leaflet_overview(lga);
+  }
 
-	function lga_sector(lga, sector){
-		render('#lga_sector_template', {
-			lga: lga.lga_data,
-			sector: sector
-		});
-	}
+  function lga_sector(lga, sector){
+    render('#lga_sector_template', {
+      lga: lga.lga_data,
+      sector: sector
+    });
+  }
 
-	function facility_overview(lga){
-		render('#lga_sector_template', {
-			lga: lga.lga_data,
-			facilities: facilities,
-		});
-	}
+  function facility_overview(lga){
+    render('#lga_sector_template', {
+      lga: lga.lga_data,
+      facilities: facilities
+    });
+  }
 
 
-	// Helper Functions
-	function route(view, sector){
-		return function(unique_lga){
-			var lga = NMIS.lgas[unique_lga];
-			if (typeof lga === 'undefined'){
-				var url = '/data/new_data/lgas/' + unique_lga + '.json';
-				$.getJSON(url, function(lga){
-					NMIS.lgas[unique_lga] = lga;
-					view(lga, sector);
-				});
-			} else {
-				view(lga, sector);
-			}
-		}
-	}
+  // Helper Functions
+  function route(view, sector){
+    return function(unique_lga){
+      var lga = NMIS.lgas[unique_lga];
+      if (typeof lga === 'undefined'){
+        var url = '/data/new_data/lgas/' + unique_lga + '.json';
+        $.getJSON(url, function(lga){
+          NMIS.lgas[unique_lga] = lga;
+          view(lga, sector);
+        });
+      } else {
+        view(lga, sector);
+      }
+    }
+  }
 
-	function render(template_id, context){
-		var template = $(template_id).html();
-		context.NMIS = NMIS;
-		context.format_value = format_value;
-		var html = _.template(template, context);
-		$('#content .content').html(html);
-	}
+  function render(template_id, context){
+    var template = $(template_id).html();
+    context.NMIS = NMIS;
+    context.format_value = format_value;
+    var html = _.template(template, context);
+    $('#content .content').html(html);
+  }
 
-	function format_value(value){
-		if (typeof value === 'undefined') return '-';
-		if (_.isNumber(value) && value % 1 !== 0)
-			return value.toFixed(2);
-		return value;
-	}
+  function format_value(value){
+    if (typeof value === 'undefined') return '-';
+    if (_.isNumber(value) && value % 1 !== 0)
+      return value.toFixed(2);
+    return value;
+  }
 })();
 
 
