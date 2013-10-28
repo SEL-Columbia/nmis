432f78b7dcb53a1f16c0bb05322245f6e3d803c8
Prabhas Pokharel <prabhas.pokharel@gmail.com>
2013-10-28 18:16:33 -0400

    a map whose layers can be toggled by a function (changeIndicator)

--- static/js/mdgs.js
+++ static/js/mdgs.js
@@ -1,22 +1,44 @@
 $(function(){
-    leaflet_mdgs();
+    var globalMap = map = newMDGsMap();
+//    var tileLayers = {},
+    tileLayers = {};
+
+
+    initTileLayersFromIndicatorNames(
+        ['NMIS_gross_enrollment_ratio_secondary_education'],
+        map);
+
+    
+    // Creates a new MDGs map (with nothing but centering information)
+    function newMDGsMap() {
+        //ex: NMIS_gross_enrollment_ratio_secondary_education
+        var centroid = {lat: 9.16718, lng: 7.53662};
+        var mapZoom = 6;
+        return L.map('mdg-map', {
+            center: new L.LatLng(centroid.lat, centroid.lng),
+            zoom: mapZoom,
+        });
+    }
+
+    // Adds a tile layer per indicator in indicatorNames, as well as an
+    // extra: nigeria_base
+    function initTileLayersFromIndicatorNames(indicatorNames, map) {
+        var mapboxURL = 'https://{s}.tiles.mapbox.com/v3/{user}.{map}/{z}/{x}/{y}.png';
+        var layers = indicatorNames.concat(['nigeria_base']);
+        layers.forEach(function(layerName) {
+            var thisLayer = L.tileLayer(mapboxURL, 
+                                    {user: 'modilabs', map: layerName})
+                            .addTo(map);
+            tileLayers[layerName] = thisLayer;
+        });
+    }
+
+    // Change indicator layer
+    function changeIndicator(indicatorName) {
+        tileLayers[indicatorName].bringToFront();
+    }
 });
+var globalMap, tileLayers;
 
-centroid = {lat: 9.16718,
-                 lng: 7.53662};
 
-function leaflet_mdgs(){
-    var map_div = $(".mdg-map")[0];
-    var lat_lng = new L.LatLng(centroid.lat, centroid.lng);
-    var map_zoom = 6;
-    var summary_map = L.map(map_div, {})
-            .setView(lat_lng, map_zoom);
-    var tileset = "nigeria_base";
-    var tile_server = "http://{s}.tiles.mapbox.com/v3/modilabs." +
-                      tileset +
-                      "/{z}/{x}/{y}.png";
-    L.tileLayer(tile_server, {
-        minZoom: 6,
-        maxZoom: 11
-    }).addTo(summary_map);
-}
+
