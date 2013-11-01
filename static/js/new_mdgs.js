132478da57b101eb490b15678a3da7801dd538e4
Myf Ma <myf@worldtalks.org>
2013-11-01 11:26:02 -0400

    mdgs pages

--- static/js/new_mdgs.js
+++ static/js/new_mdgs.js
@@ -1,27 +1,34 @@
 $(function(){
-    var map = mapInit();
+    window.map = mapInit();
+    mapLayerInit(map, 'NMIS_gross_enrollment_ratio_secondary_education');
 });
 
-var mapboxTileServer = function(indicator) {
-    return 'http://{s}.tiles.mapbox.com/v3/modilabs.' +
+var mapboxTileLayer = function(indicator, minZoom, maxZoom) {
+    var tileServer = 'http://{s}.tiles.mapbox.com/v3/modilabs.' +
         indicator + '/{z}/{x}/{y}.png';
+    var tileLayer = new L.TileLayer(tileServer, {
+        minZoom: minZoom,
+        maxZoom: maxZoom
+    });
+    return tileLayer;
 };
-
 
 var mapInit = function() {
     var mapZoom = 6;
     var map_div = $('.mdg-map')[0];
     var lat_lng = new L.LatLng(9.16718, 7.53662);
-    var map = new L.Map(map_div).setView(lat_lng, mapZoom);
-    var tileset = "nigeria_base";
-    var tileServer = mapboxTileServer(tileset);
-    var baseLayer = new L.TileLayer(tileServer, {
-        minZoom: 6,
-        maxZoom: 11
-    });
+    var sw = new L.LatLng(4.039617826768437, 0.17578125);
+    var ne = new L.LatLng(14.221788628397572, 14.897460937499998);
+    var country_bounds = new L.LatLngBounds(sw, ne);
+    var map = new L.Map(map_div,{
+            maxBounds: country_bounds
+        }).setView(lat_lng, mapZoom);
+    var baseTile = "nigeria_base";
+    var baseLayer = mapboxTileLayer(baseTile, 6, 11);
     baseLayer.addTo(map);
     return map;
 };
 
-var mapLayerSwitcher = function() {
+var mapLayerInit = function(map, layers) {
+    var tempSever = mapboxTileLayer(layers, 6, 9);
 };
