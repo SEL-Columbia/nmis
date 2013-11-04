ec6c198ac21e994bd537ef8dd237f4cebb1065bd
Chris Tan <csytan@gmail.com>
2013-11-04 15:09:19 -0500

    json.loads() instead of dumps()

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def about():
 
 @app.route('/mdgs')
 def mdgs():
-    mdg_layers = json.dumps(load_json('mdg_layers'));
+    mdg_layers = json.loads(load_json('mdg_layers'));
     return flask.render_template('mdgs.html',
             mdg_layers=mdg_layers)
             
