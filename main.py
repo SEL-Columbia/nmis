8c7fc1d10461689564a2ff8f1948308b8079f244
myf <worldwidespirit@gmail.com>
2013-11-06 15:14:41 -0500

    bug since main load_static_data changed

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def about():
 
 @app.route('/mdgs')
 def mdgs():
-    mdg_layers = json.loads(load_static_data('mdg_layers'));
+    mdg_layers = json.loads(load_static_data('mdg_layers.json'));
     return flask.render_template('mdgs.html',
             mdg_layers=mdg_layers)
             
