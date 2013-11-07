a3ae6ce4037a1eae8e7c4875e7891dc97944cefa
myf <worldwidespirit@gmail.com>
2013-11-07 15:56:17 -0500

    mdgs.js no more global variables, binds to map div

--- static/mdgs.js
+++ static/mdgs.js
@@ -1,23 +1,13 @@
 $(function(){
-    var map = mapInit();
-    var mapLayers = {};
-    var currentLayer = {};
+    var map_div = $('.mdg-map')[0];
+    mapInit(map_div);
     $('#mdg-selector').selectize({
         onItemAdd: function(value){
-            addMapLayer(map, value);
+            applyMap(map_div, value);
+            applyDescription(value);
         }
     });
 });
-
-var mapboxTileLayer = function(indicator, minZoom, maxZoom) {
-    var tileServer = 'http://{s}.tiles.mapbox.com/v3/modilabs.' +
-        indicator + '/{z}/{x}/{y}.png';
-    var tileLayer = new L.TileLayer(tileServer, {
-        minZoom: minZoom,
-        maxZoom: maxZoom
-    });
-    return tileLayer;
-};
 
 var mapboxLayer = function(indicator, minZoom, maxZoom) {
     var mapboxName = 'modilabs.' + indicator;
@@ -28,9 +18,8 @@ var mapboxLayer = function(indicator, minZoom, maxZoom) {
     return tileLayer;
 };
 
-var mapInit = function() {
+var mapInit = function(map_div) {
     var mapZoom = 6;
-    var map_div = $('.mdg-map')[0];
     var lat_lng = new L.LatLng(9.16718, 7.53662);
     var sw = new L.LatLng(4.039617826768437, 0.17578125);
     var ne = new L.LatLng(14.221788628397572, 14.897460937499998);
@@ -40,19 +29,39 @@ var mapInit = function() {
         }).setView(lat_lng, mapZoom);
     var baseLayer = mapboxLayer('nigeria_base', 6, 9);
     baseLayer.addTo(map);
-    return map;
+    map_div.map = map;
+    map_div.mapLayers = {};
+    map_div.currentLayer = {};
 };
 
-var cleanMap = function(map) {
+var cleanMap = function(map_div) {
+    var currentLayer = map_div.currentLayer;
+    var map = map_div.map;
+    var legend = currentLayer.legend;
+    var layer = currentLayer.layer;
+    legend.removeFrom(map);
+    map.removeLayer(layer);
 };
 
 var addMapLayer = function(map, layer) {
-    var tempLayer = mapboxLayer(layer, 6, 9);
     var legend = L.mapbox.legendControl();
+    var tempLayer = mapboxLayer(layer, 6, 9);
     tempLayer.on('ready', function(){
         var TileJSON = tempLayer.getTileJSON();
         legend.addLegend(TileJSON.legend);
-        legend.addTo(map);
     });
-    tempLayer.addTo(map);
+    return {layer: tempLayer, legend: legend};
+};
+
+var applyMap = function(map_div, value) {
+    if(!(_.isEmpty(map_div.currentLayer))) { cleanMap(map_div); }
+    if(!(map_div.mapLayers[value])) {
+        map_div.mapLayers[value] = addMapLayer(map_div.map, value);
+    }
+    map_div.currentLayer = map_div.mapLayers[value];
+    map_div.map.addLayer(map_div.mapLayers[value].layer);
+    map_div.mapLayers[value].legend.addTo(map_div.map);
+};
+
+var applyDescription = function(value) {
 };
