98cc0e6da8efcdd70c6e33cfa922c569aa039913
Myf Ma <myf@worldtalks.org>
2013-11-18 19:25:12 -0500

    updating zone images, closes #71

---
+++ static/images/zones/downloa_new_pic.sh
@@ -0,0 +1,6 @@
+wget https://f.cloud.github.com/assets/4739887/1564702/3854997c-5073-11e3-847e-1f4059f164fb.png
+wget https://f.cloud.github.com/assets/4739887/1564704/3854d2b6-5073-11e3-8560-6a70914013db.png
+wget https://f.cloud.github.com/assets/4739887/1564706/3856c4d6-5073-11e3-8813-30f3c4723897.png
+wget https://f.cloud.github.com/assets/4739887/1564703/38549c2e-5073-11e3-889e-ae3254cccd2f.png
+wget https://f.cloud.github.com/assets/4739887/1564705/3855bf00-5073-11e3-9e5b-205a9d3c0c71.png
+wget https://f.cloud.github.com/assets/4739887/1564707/38570d88-5073-11e3-92e8-0d1828f19d33.png
