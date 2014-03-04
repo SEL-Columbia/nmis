49b000c2a962675c4161445b48082f3bed8d9c6c
Chris Tan <csytan@gmail.com>
2014-03-04 15:48:45 -0500

    Cron script for NMIS git updates

---
+++ server_config/update_dev.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Update NMIS repo
+cd /home/ubuntu/srv/nmis
+git pull
+
+# Update NMIS data
+cd /home/ubuntu/srv/nmis_ui_data_2ef92c15
+
+# Run csv2json if needed
+git_status=`git status -s | wc -l`
+if [ $git_status -ne 0 ]; then
+    do
+        git pull
+        python csv2json.py >> /tmp/csv2json.log 2>&1
+    done
+fi
+
+
