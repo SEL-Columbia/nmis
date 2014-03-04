a72babd3e8a52b6b2e6acf624c694abd798cf795
Chris Tan <csytan@gmail.com>
2014-03-04 16:23:19 -0500

    Clean up comments

--- server_config/update_dev.sh
+++ server_config/update_dev.sh
@@ -4,10 +4,9 @@
 cd /home/ubuntu/srv/nmis
 git pull
 
-# Update NMIS data
+# Update NMIS data and run csv2json if needed
 cd /home/ubuntu/srv/nmis_ui_data_2ef92c15
 
-# Run csv2json if needed
 git_result=`git pull | grep "^Already up-to-date." | wc -l`
 if [ $git_result -eq 0 ]; then
     python csv2json.py >> /tmp/csv2json.log 2>&1
