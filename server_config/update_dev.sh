3210c210e19b1e162cbbee30a579493f6b9464f2
Chris Tan <csytan@gmail.com>
2014-03-04 16:19:27 -0500

    Fixing cron update script

--- server_config/update_dev.sh
+++ server_config/update_dev.sh
@@ -6,12 +6,10 @@ git pull
 
 # Update NMIS data
 cd /home/ubuntu/srv/nmis_ui_data_2ef92c15
-cd data_774
 
 # Run csv2json if needed
-git_status=`git status . -s | wc -l`
-if [ $git_status -ne 0 ]; then
-    cd ..
+git_result=`git pull | grep "^Already up-to-date." | wc -l`
+if [ $git_result -eq 0 ]; then
     git pull
     python csv2json.py >> /tmp/csv2json.log 2>&1
 fi
