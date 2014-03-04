33b9c4d71a00ddf836d034b67170aaa4f42d0207
Chris Tan <csytan@gmail.com>
2014-03-04 16:00:36 -0500

    Checks for changes in the data_774 directory

--- server_config/update_dev.sh
+++ server_config/update_dev.sh
@@ -6,10 +6,12 @@ git pull
 
 # Update NMIS data
 cd /home/ubuntu/srv/nmis_ui_data_2ef92c15
+cd data_774
 
 # Run csv2json if needed
-git_status=`git status -s | wc -l`
+git_status=`git status . -s | wc -l`
 if [ $git_status -ne 0 ]; then
+    cd ..
     git pull
     python csv2json.py >> /tmp/csv2json.log 2>&1
 fi
