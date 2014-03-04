ee649305b02eb9bb967089b3c177c95fe58a61eb
Chris Tan <csytan@gmail.com>
2014-03-04 16:21:55 -0500

    This should work

--- server_config/update_dev.sh
+++ server_config/update_dev.sh
@@ -10,7 +10,6 @@ cd /home/ubuntu/srv/nmis_ui_data_2ef92c15
 # Run csv2json if needed
 git_result=`git pull | grep "^Already up-to-date." | wc -l`
 if [ $git_result -eq 0 ]; then
-    git pull
     python csv2json.py >> /tmp/csv2json.log 2>&1
 fi
 
