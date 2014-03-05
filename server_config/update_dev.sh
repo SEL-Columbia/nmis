5a350096f10231c616afb6b3687cd89b5ca92da8
Chris Tan <csytan@gmail.com>
2014-03-05 13:12:55 -0500

    Better logging

--- server_config/update_dev.sh
+++ server_config/update_dev.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
+log="/tmp/update_dev.log"
+
+date >> $log
+
 # Update NMIS repo
 cd /home/ubuntu/srv/nmis
-git pull >> /tmp/update_dev.log
+git pull >> $log
 
 # Update NMIS data and run csv2json if needed
 cd /home/ubuntu/srv/nmis_ui_data_2ef92c15
 
 
 git_result=`git pull | grep "^Already up-to-date." | wc -l`
-echo $git_result >> /tmp/update_dev.log 
+echo $git_result >> $log
 if [ $git_result -eq 0 ]; then
-    python csv2json.py >> /tmp/update_dev.log 2>&1
+    python csv2json.py >> $log
 fi
 
+echo "--------------------------------------------------" >> $log
 
