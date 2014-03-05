da42f6542374b97fa5d83bb9691c8039b7a60222
Chris Tan <csytan@gmail.com>
2014-03-05 13:05:10 -0500

    Logging for the update script

--- server_config/update_dev.sh
+++ server_config/update_dev.sh
@@ -2,14 +2,16 @@
 
 # Update NMIS repo
 cd /home/ubuntu/srv/nmis
-git pull
+git pull >> /tmp/update_dev.log
 
 # Update NMIS data and run csv2json if needed
 cd /home/ubuntu/srv/nmis_ui_data_2ef92c15
 
+
 git_result=`git pull | grep "^Already up-to-date." | wc -l`
+echo $git_result >> /tmp/update_dev.log 
 if [ $git_result -eq 0 ]; then
-    python csv2json.py >> /tmp/csv2json.log 2>&1
+    python csv2json.py >> /tmp/update_dev.log 2>&1
 fi
 
 
