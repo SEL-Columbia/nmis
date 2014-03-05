ce42eb1d764693bd38f8d868e63611c382a334f6
Chris Tan <csytan@gmail.com>
2014-03-05 13:38:40 -0500

    Better logging

--- server_config/update_dev.sh
+++ server_config/update_dev.sh
@@ -6,17 +6,20 @@ date >> $log
 
 # Update NMIS repo
 cd /home/ubuntu/srv/nmis
+echo "Updating NMIS repo" >> $log
 git pull >> $log
 
 # Update NMIS data and run csv2json if needed
 csv2json=`ps aux | grep "python csv2json.py" | wc -l`
 if [ $csv2json -eq 1 ]; then
+    echo "Updating NMIS-data repo" >> $log
     cd /home/ubuntu/srv/nmis_ui_data_2ef92c15
 
     git_result=`git pull | grep "^Already up-to-date." | wc -l`
     echo $git_result >> $log
-    
+
     if [ $git_result -eq 0 ]; then
+        echo "Running csv2json" >> $log
         python csv2json.py >> $log
 
         # Remove old LGAs folder
