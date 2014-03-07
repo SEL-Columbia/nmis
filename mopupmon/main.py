558c7ce9efd270ab3a03be0e6f94ba95cd6534a7
Chris Tan <csytan@gmail.com>
2014-03-06 19:12:15 -0500

    More work on mop-up monitor

--- mopupmon/main.py
+++ mopupmon/main.py
@@ -12,39 +12,100 @@ CWD = os.path.dirname(os.path.abspath(__file__))
 
 
 def get_mopup_data():
-    url = "https://formhub.org/ossap/forms/mopup_questionnaire_health_final/api?fields=%5B%22lga%22%2C+%22facility_ID%22%5D"
+    """
+    Returns a set of facility ids that have been surveyed.
 
+    set(["hcho", "nlqf", "xjeh"])
+    """
+    url = "https://formhub.org/ossap/forms/mopup_questionnaire_health_final/api?fields=%5B%22lga%22%2C+%22facility_ID%22%5D"
+    
     request = urllib2.Request(url)
     base64string = base64.encodestring('%s:%s' % (secrets.username, secrets.password)).replace('\n', '')
     request.add_header("Authorization", "Basic %s" % base64string)   
     response = urllib2.urlopen(request)
-    data = response.read()
-    return json.loads(data)
+    facilities = json.loads(response.read())
 
-def parse_csv(file):
-    print 'Parsing: ' + file.name
-    output = []
-    reader = csv.reader(file, delimiter=',', quotechar='"')
-    headers = [h.strip('"') for h in reader.next()]
-    for row in reader:
-        value = {}
-        for header, col in zip(headers, row):
-            value[header.lower()] = col
-        output.append(value)
-    return output
+    return set(f['facility_ID'].lower() for f in facilities if 'facility_ID' in f)
 
 
-def load_facilities():
-    results = []
+def parse_facilities(surveyed=set()):
+    """
+    Parses the education and health CSVs and returns the data structure below.
+
+    Keyword arguments:
+    surveyed (optional) -- a set of facility ids that have been surveyed
+
+    Returns:
+    {
+        "imo_oru_west": {
+            "surveyed": ["Saint Andrews", "Government School"],
+            "unsurveyed": ["AFONORI Primary School", "FOGUWA Primary School", "MARI Primary School"],
+            "percent_complete": 40
+            ...
+        }
+    }
+    """
+    # Parse CSV files
+    facilities = []
     for fname in os.listdir(CWD):
-        file_path = os.path.join(CWD, fname)
-        if fname.endswith('.csv'):
-            with open(file_path, 'rb') as f:
-                result = parse_csv(f)
-                results += result
-    print results
-    return results
+        if not fname.endswith('.csv'):
+            continue
 
+        file_path = os.path.join(CWD, fname)            
+        with open(file_path, 'rb') as f:
+            print 'Parsing: ' + f.name
+
+            reader = csv.reader(f, delimiter=',', quotechar='"')
+            headers = [h.strip('"') for h in reader.next()]
+
+            for row in reader:
+                facility = {}
+                for header, col in zip(headers, row):
+                    facility[header.lower()] = col
+                facilities.append(facility)
+    
+    # Build output data structure
+    lgas = {}
+    for fac in facilities:
+        lga_id = fac['unique_lga']
+        fac_id = fac['uid']
+        fac_name = fac['name']
+
+        lga = lgas.setdefault(lga_id, {})
+        surveyed = lga.setdefault('surveyed', [])
+        unsurveyed = lga.setdefault('unsurveyed', [])
+
+        if fac_id in surveyed:
+            surveyed.append(fac_name)
+        else:
+            unsurveyed.append(fac_name)
+
+        lga['percent_complete'] = int(len(surveyed) / (len(surveyed) + len(unsurveyed)) * 100)
+        lga_facs[fac_id] = fac
+
+    return lgas
+
+
+
+def combine_data(facilities, surveyed):
+    """
+    Keyword arguments:
+    zones -- zones data
+
+    Structure of an LGA's data:
+    {
+        "id": "gombe_kwami",
+        "name": "Kwami",
+        "surveyed": ["Saint Andrews", "Government School"],
+        "unsurveyed": ["AFONORI Primary School", "FOGUWA Primary School", "MARI Primary School"],
+        "percent_complete": 40
+    }
+    """
+
+
+surveyed = get_mopup_data()
+lgas = parse_facilities(surveyed)
+print lgas
 
 def zones():
     """
@@ -67,15 +128,6 @@ def zones():
             ]
         }
     ]
-
-    Structure of an LGA's data:
-    {
-        'id': 'gombe_kwami',
-        'name': 'Kwami',
-        'surveyed': ["Saint Andrews", "Government School"],
-        'unsurveyed': ["AFONORI Primary School", "FOGUWA Primary School", "MARI Primary School"],
-        'percent_complete': 40
-    }
     """
 
     file_path = os.path.join(CWD, 'zones.json')
@@ -86,9 +138,6 @@ def zones():
         zone
 
     print data
-
-
-zones()
 
 
 
