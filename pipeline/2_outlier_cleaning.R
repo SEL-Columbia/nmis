c0b79146222c34be21d822c373426b3cbf427274
prabhasp <prabhas.pokharel@gmail.com>
2014-05-06 15:50:42 -0400

    cleaning up re-produced outlier code; began small re-structuring of health outliers

--- pipeline/2_outlier_cleaning.R
+++ pipeline/2_outlier_cleaning.R
@@ -3,6 +3,7 @@
 ######################################################################################################
 require(dplyr)
 source('nmis_functions.R')
+
 education_outlier <- function(education_data) {
     return(education_data 
         %.% mutate(
@@ -22,6 +23,14 @@ education_outlier <- function(education_data) {
 }
 
 health_outlier <- function(health_data) {
+    hospital_outlier_repalced <- health_data %.% filter(
+        facility_type %in% c("teaching_hospital", "district_hospital")
+    ) %.% mutate(
+    )
+    non_hospital_outlier_replaced = health_data %.% filter(
+    )
+    rbind(hospital_outlier_replaced, non_hospital_outlier_replaced)
+    
     return(health_data 
             %.% mutate(
 
@@ -77,33 +86,6 @@ health_outlier <- function(health_data) {
     )
 }
 
-# #calling source script
-#   source('nmis_functions.R')
-#   library(stats)
-#   source('CONFIG.R')
-# 
-# #reading in data
-#   edu_merged <- readRDS("data/in_process_data/education_mopup_merged.RDS")
-#   health_merged <- readRDS("data/in_process_data/health_mopup_merged.RDS")
-# 
-# #education############################################################################################
-# 
-# #logic checks
-#   edu_merged <- outlierreplace(edu_merged, 'num_tchrs_male', 
-#                                    (edu_merged$num_tchrs_male > edu_merged$num_tchrs_total))
-# 
-#   edu_merged <- outlierreplace(edu_merged, 'num_tchrs_female', 
-#                                    (edu_merged$num_tchrs_female > edu_merged$num_tchrs_total))
-# 
-#   edu_merged <- outlierreplace(edu_merged, 'num_tchrs_with_nce',
-#                                    (edu_merged$num_tchrs_with_nce > edu_merged$num_tchrs_total))
-# 
-#   edu_merged <- outlierreplace(edu_merged, 'num_classrms_repair', 
-#                                    edu_merged$num_classrms_repair > edu_merged$num_classrms_total) 
-# 
-#   edu_merged <- outlierreplace(edu_merged, 'num_tchrs_total',
-#                                    (edu_merged$num_tchrs_total > edu_merged$num_tchrs_male + 
-#                                       edu_merged$num_tchrs_female))
 # #outliers
 #   edu_merged <- outlierreplace(edu_merged, 'num_students_total', 
 #                                    edu_merged$num_students_total == 0)    
@@ -128,21 +110,6 @@ health_outlier <- function(health_data) {
 #                                    (edu_merged$num_classrms_total == 0))
 # 
 # #further inspection/determining of cut off
-# 
-# #   edu_merged <- outlierreplace(edu_merged, 'num_tchrs_male',
-# #                                    (edu_merged$num_tchrs_male > 100))
-# 
-# #   edu_merged <- outlierreplace(edu_merged, 'num_tchrs_female',
-# #                                    (edu_merged$num_tchrs_female > 100))
-# 
-# #   edu_merged <- outlierreplace(edu_merged, 'num_tchrs_with_nce',
-# #                                    (edu_merged$num_tchrs_with_nce > 100))
-# 
-# #   edu_merged <- outlierreplace(edu_merged, 'num_classrms_repair',
-# #                                    (edu_merged$num_classrms_repair > 50))
-# 
-# #   edu_merged <- outlierreplace(edu_merged, 'num_students_total',
-# #                                    edu_merged$num_students_total > 2355)
 # 
 # #   edu_merged <- outlierreplace(edu_merged, 'num_toilets_total',
 # #
@@ -172,110 +139,4 @@ health_outlier <- function(health_data) {
 # 
 #   edu_merged <- outlierreplace(edu_merged, 'ratio_students_to_toilet',
 #                                    between(edu_merged$ratio_students_to_toilet, 1000, Inf))   
-# 
-# #writing out
-#   saveRDS(edu_merged, "data/in_process_data/education_mopup_outliercleaned.rds")
-# 
-# #health###############################################################################################
-# 
-# #outliers
-# #   health_merged <-outlierreplace(health_merged, 'num_doctors_fulltime',
-# #                                      (health_merged$num_doctors_fulltime > 12 & 
-# #                                         (health_merged$facility_type != "teaching_hospital" & 
-# #                                            health_merged$facility_type != "district_hospital")))
-# 
-# #   health_merged <- outlierreplace(health_merged, 'num_doctors_fulltime',
-# #                                       (health_merged$num_doctors_fulltime > 20 & 
-# #                                          (health_merged$facility_type == "teaching_hospital" | 
-# #                                             health_merged$facility_type == "district_hospital")))
-# 
-# #   health_merged <- outlierreplace(health_merged, 'num_nurses_fulltime',
-# #                                       (health_merged$num_nurses_fulltime > 16 & 
-# #                                          (health_merged$facility_type != "teaching_hospital" & 
-# #                                             health_merged$facility_type != "district_hospital")))
-# 
-# #   health_merged <- outlierreplace(health_merged, 'num_nurses_fulltime',
-# #                                       (health_merged$num_nurses_fulltime > 24 & 
-# #                                          (health_merged$facility_type == "teaching_hospital" | 
-# #                                             health_merged$facility_type == "federalmedicalcentre")))
-# 
-#   health_merged <- outlierreplace(health_merged, 'num_midwives_fulltime',
-#                                       (health_merged$num_midwives_fulltime > 24 & 
-#                                          (health_merged$facility_type == "teaching_hospital" | 
-#                                             health_merged$facility_type == "district_hospital")))
-# 
-# #   health_merged <- outlierreplace(health_merged, 'num_midwives_fulltime',
-# #                                       (health_merged$num_midwives_fulltime > 16 & 
-# #                                          (health_merged$facility_type != "teaching_hospital" & 
-# #                                             health_merged$facility_type != "district_hospital")))
-# 
-# #   health_merged <- outlierreplace(health_merged, 'facility_type',
-# #                                       (((health_merged$num_doctors_fulltime < 30 & 
-# #                                            health_merged$num_doctors_fulltime != 0) & 
-# #                                           (health_merged$num_midwives_fulltime < 30 & 
-# #                                              health_merged$num_midwives_fulltime != 0) &
-# #                                           (health_merged$num_nurses_fulltime < 30 & 
-# #                                              health_merged$num_nurses_fulltime != 0)) &
-# #                                          (health_merged$facility_type == "teaching_hospital" | 
-# #                                             health_merged$facility_type == "district_hospital")))
-# 
-# #   health_merged <- outlierreplace(health_merged, 'num_doctors_fulltime',
-# #                                       ((health_merged$num_doctors_fulltime > 500 | 
-# #                                           health_merged$num_doctors_fulltime < 100) & 
-# #                                          (health_merged$facility_type == "teaching_hospital"  |
-# #                                             health_merged$facility_type == "district_hospital")
-# #                                       ))
-# 
-# #   health_merged <- outlierreplace(health_merged, 'num_nurses_fulltime',
-# #                                       (health_merged$num_nurses_fulltime < 100 &
-# #                                          (health_merged$facility_type == "teaching_hospital" | 
-# #                                             health_merged$facility_type == "district_hospital")))
-# 
-# #   health_merged <- outlierreplace(health_merged, 'num_nurses_fulltime',
-# #                                       (health_merged$num_nurses_fulltime > 16 & 
-# #                                          (health_merged$facility_type != "teaching_hospital" & 
-# #                                             health_merged$facility_type != "district_hospital")))            
-# 
-# #   health_merged <- outlierreplace(health_merged, 'num_midwives_fulltime',
-# #                                       (health_merged$num_midwives_fulltime < 100 & 
-# #                                          (health_merged$facility_type == "teaching_hospital" | 
-# #                                             health_merged$facility_type == "district_hospital")))
-# 
-#   
-# #   health_merged <- outlierreplace(health_merged, 'num_midwives_fulltime',
-# #                                       (health_merged$num_midwives_fulltime > 16 & 
-# #                                          (health_merged$facility_type != "teaching_hospital" & 
-# #                                             health_merged$facility_type != "district_hospital")))
-# 
-# #   health_merged <- outlierreplace(health_merged, 'num_chews_fulltime',
-# #                                       (health_merged$num_chews_fulltime > 50 & 
-# #                                          (health_merged$facility_type != "teaching_hospital" & 
-# #                                             health_merged$facility_type != "district_hospital")))
-# 
-# #   health_merged <- outlierreplace(health_merged, 'num_chews_fulltime',
-# #                                       (health_merged$num_chews_fulltime > 50 &
-# #                                          (health_merged$facility_type == "teaching_hospital" | 
-# #                                             health_merged$facility_type == "district_hospital")))
-# 
-# #further inspection/determining of cut off
-# #   health_merged <- outlierreplace(health_merged, 'num_toilets_total',
-#                                   
-# 
-# #writing out
-#   saveRDS(health_merged, "data/in_process_data/health_mopup_outliercleaned.rds")
-# 
-# # library(ggplot2)
-# # cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
-# # ggplot(edu_merged, aes(x=zone, y=num_tchrs_male, fill=zone)) +
-# #   coord_flip() + geom_boxplot() + ylab('Number of Male Teachers') + xlab('Zone') + 
-# #   scale_fill_manual(values=cbPalette) #+ scale_y_continuous(limits=c(0,3000))
-# #
-# # quantile(health_merged$num_chews_fulltime, na.rm=T, 0.999)
-# 
-# 
-#   remove(between)
-#   remove(outlierreplace)
-#   remove(edu_merged)
-#   remove(health_merged)
-# 
 # 
