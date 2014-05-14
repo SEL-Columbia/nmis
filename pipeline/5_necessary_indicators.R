01c62e32523d22decd50c80008a4e4cce6d94338
Myf Ma <myf@worldtalks.org>
2014-05-14 17:47:43 -0400

    adding modularized 5_necessary_indicators.R before output

---
+++ pipeline/5_necessary_indicators.R
@@ -0,0 +1,14 @@
+######################################################################################################
+#Mopup Integration: clean up before final output Cleaning##################################################################
+######################################################################################################
+require(dplyr)
+source('nmis_functions.R')
+source('CONFIG.R')
+output_indicators <- function(df, lgas, level, sector) {
+    colnames(df)[colnames(df)=='unique_lga'] <- 'unique_lga_2013'
+    df <- df %.% join(lgas, by='unique_lga_2013')
+    df <- df[get_necessary_indicators()[level][sector]]
+    return(df)
+}
+
+
