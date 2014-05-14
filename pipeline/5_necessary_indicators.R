e30a1cca7467ae5adde5aa97385d842ed483814f
Myf Ma <myf@worldtalks.org>
2014-05-14 17:57:46 -0400

    R vetorized lists requires double bracket

--- pipeline/5_necessary_indicators.R
+++ pipeline/5_necessary_indicators.R
@@ -7,7 +7,7 @@ source('CONFIG.R')
 output_indicators <- function(df, lgas, level, sector) {
     colnames(df)[colnames(df)=='unique_lga'] <- 'unique_lga_2013'
     df <- df %.% join(lgas, by='unique_lga_2013')
-    df <- df[get_necessary_indicators()[level][sector]]
+    df <- df[get_necessary_indicators()[[level]][[sector]]]
     return(df)
 }
 
