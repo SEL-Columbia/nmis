1c6c6f66326a97a57ec1e048888e29f006ec00ab
zmyao88 <zm.yao88@gmail.com>
2014-07-11 00:33:58 -0400

    check R version implemented

--- pipeline/setup.R
+++ pipeline/setup.R
@@ -1,5 +1,19 @@
+check_r_version <- function(){
+    if(R.version$major < 3){
+        stop("Your R Version it too low, Upgrade to 3.0.1")
+    }else{
+        if(R.version$minor < 1.0){
+            stop("Your R Version it too low, Upgrade to 3.0.1")
+        }
+    }   
+}
+check_r_version()
 required_pkgs <- list("dplyr" = "0.2", "lubridate" = "1.3.3", 
-                      )
+                      "RSQLite" = "0.11.4", "RSQLite" = "0.0.1",
+                      "plyr" = "1.8.1", "stringr" = "0.6.2",
+                      "rjson" = "0.2.13", "foreach" = "1.4.2",
+                      "doMC" = "1.3.3", "fpc" = "2.1-7",
+                      "sp" = "1.0-15")
 
 install_n_load <- function(required_pkgs){
     installed <- installed.packages()[,3]
@@ -24,5 +38,6 @@ batch_load <- function(required_pkgs){
 }
 
 install_n_load(required_pkgs)
-batch_load(required_pkgs)
-rm(install_n_load, batch_load, required_pkgs)
+# batch_load(required_pkgs)
+rm(install_n_load, batch_load, required_pkgs, check_r_version)
+
