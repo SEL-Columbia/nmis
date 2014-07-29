f22faf46ca981df6b40c1e7ac82534377dd54b17
Myf Ma <myf@worldtalks.org>
2014-07-29 13:50:10 -0400

    escape $ in config

--- server_config/setup.sh
+++ server_config/setup.sh
@@ -86,7 +86,7 @@ write_nginx_config() {
     echo "" >> $file_uri
     echo "	location ^~ /static/ {" >> $file_uri
     echo "		root $nmis_dir;" >> $file_uri
-    echo "		if ($query_string) {" >> $file_uri
+    echo "		if (\$query_string) {" >> $file_uri
     echo "			expires max;" >> $file_uri
     echo "		}" >> $file_uri
     echo "	}" >> $file_uri
