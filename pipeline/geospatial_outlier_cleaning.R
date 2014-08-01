b6e622a883717f9aae6fcd105179aa5997f84c41
zmyao88 <zm.yao88@gmail.com>
2014-08-01 15:38:48 -0400

    removed duplicates from coby and baseline list

--- pipeline/geospatial_outlier_cleaning.R
+++ pipeline/geospatial_outlier_cleaning.R
@@ -18,7 +18,7 @@ cluster_lga <- function(lat, lon, plot_lga=F){
   if (nrow(gps_coords) <= 1){ return(F) }
   # computes the pairwise distances (euclidean) between all points in the lga, 
   # selects a triangle, ravels to a vector and computes the 50% quantile
-  epsilon <- as.numeric(quantile(as.vector(dist(gps_coords)), .5, na.rm=T))
+  epsilon <- as.numeric(quantile(as.vector(dist(gps_coords)), .25, na.rm=T))
   # fits the DBSCAN model with the normalized coords and epsilon from above 
   DBSCAN <- fpc::dbscan(gps_coords, eps=epsilon, MinPts = 3)
   gps_coords$cluster <- DBSCAN$cluster
@@ -43,26 +43,28 @@ cluster_lga <- function(lat, lon, plot_lga=F){
   
   #if set to true plot the facilities, colored by group [spatial_outlier, !spatial_outlier]
   if (plot_lga == T){
-    print(ggplot(gps_coords, aes(x=longitude, y=latitude, colour=spatial_outlier)) +
-            geom_point(size=3))
+      print(ggplot(gps_coords, aes(x=longitude, y=latitude, colour=spatial_outlier)) +
+          geom_point(size=3) + 
+          scale_x_continuous(breaks=seq(floor(min(gps_coords$longitude)), ceiling(max(gps_coords$longitude)), 1)) + 
+          scale_y_continuous(breaks=seq(floor(min(gps_coords$latitude)), ceiling(max(gps_coords$latitude)), 1)))
   }
   
   return(in_hull < 1)
 }
 
-flag_spatial_outliers <- function(facility.data, output_dir, keep_outliers=F){
-  #This function takes a facility.data data.frame and tests for spatial outliers
+flag_spatial_outliers <- function(facility_data, output_dir, keep_outliers=F){
+  #This function takes a facility_data data.frame and tests for spatial outliers
   #output_dir is where you want the spatial_outlier csv to be saved
   #if keep_outliers is set to True, then no data is dropped the input frame is returned 
   # with a column of T or F appended describing whether it is an outlier
   
   #Takes the input frame, groups by unique_lga and then adds an outlier column
-  facility.data <- facility.data %.%
+  facility_data <- facility_data %.%
     dplyr::group_by(unique_lga) %.%
     dplyr::mutate(spatial_outlier=cluster_lga(latitude, longitude))
   
   #filters the above dataframe for spatial_outliers
-  spatial_outliers <- dplyr::filter(facility.data, spatial_outlier==T)
+  spatial_outliers <- dplyr::filter(facility_data, spatial_outlier==T)
   #persists those spatial outliers to disk as csv
   write.csv(spatial_outliers, row.names=F,
             file=sprintf('%s/Spatial_Outliers_Mopup_and_Baseline_NMIS_Facility.csv', output_dir))
@@ -70,9 +72,9 @@ flag_spatial_outliers <- function(facility.data, output_dir, keep_outliers=F){
   # if keep outliers is True, dont drop the outliers, just add the outlier column and return
   if (keep_outliers == F){  
     spatial_outliers <- dplyr::select(spatial_outliers, -spatial_outlier)
-    facility.data <- dplyr::filter(facility.data, spatial_outlier==F) %.%
+    facility_data <- dplyr::filter(facility_data, spatial_outlier==F) %.%
       dplyr::select(-spatial_outlier)
     
   }
-  return (facility.data)
+  return (facility_data)
 }
